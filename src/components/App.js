b085a5b9a56458ceeb93040b7d00ee5791bb7f91
shahbaj khan <130038256+beingshahbaj@users.noreply.github.com>
2024-01-22 12:33:44 +0000

    update

--- src/components/App.js
+++ src/components/App.js
@@ -3,6 +3,7 @@ import React from "react";
 import './../styles/App.css';
 
 const App = () => {
+  const [count , setCount] = React.useState(0);
   return (
     <div>
       <p>Button clicked {count} times</p>
